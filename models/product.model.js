930825d3e657ad5b85a1c60792a9da4f26a43dff
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-07-03 13:44:33 +0300

    Make products deletable (on backend)

--- models/product.model.js
+++ models/product.model.js
@@ -89,6 +89,15 @@ class Product {
     this.image = newImage;
     this.updateImageData();
   }
+
+  remove() {
+    const productId = new mongodb.ObjectId(this.id);
+
+    return db
+      .getDb()
+      .collection('products')
+      .deleteOne({ _id: productId });
+  }
 }
 
 module.exports = Product;
